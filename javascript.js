2bc8b73239a503d86c7b12ce1e9da6badbc3c525
Luke Coty <Lukecoty4@gmail.com>
2022-06-21 14:42:14 -0400

    added working search

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const img = document.querySelector('img');
+const btn = document.querySelector('#newImg');
+const textBox = document.querySelector('#textBox');
+
+    btn.addEventListener('click', () => {
+        const searchResult = textBox.value;
+        getNewImg(searchResult);
+    })
+
+
+    function getNewImg(searchResult) {
+      fetch(`https://api.giphy.com/v1/gifs/translate?api_key=DT0lFptRkRsItTwpuxKVdOgOLO0XQoRx&s=${searchResult}`, {mode: 'cors'})
+      .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+      img.src = response.data.images.original.url;
+    });
+    }
